70d4433465be221f8fe2efa0df6eccf49598263b
Erhan <erhantezer68@gmail.com>
2022-06-24 12:36:54 +0300

    updated

--- 6kyu_RomanNumeralsDecoder.js
+++ 6kyu_RomanNumeralsDecoder.js
@@ -19,9 +19,11 @@
 // M          1,000
 
 function solution(roman) {
-  let data = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1}, numbers = roman.split(''), sum = 0, i;
+  let data = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
+  let numbers = roman.split('');
+  let sum = 0;
   
-  for(i = 0; i < numbers.length; i++){
+  for(let i = 0; i < numbers.length; i++){
     if(data[numbers[i]] < data[numbers[i+1]]){
       sum += data[numbers[i+1]] - data[numbers[i]];
       i++;
--- 6kyu_RomanNumeralsEncoder.js
+++ 6kyu_RomanNumeralsEncoder.js
@@ -23,10 +23,15 @@
 // 
 
 function solution(number){
-  let  obj = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 }, ans = '';
+  let  obj = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
+  let ans = '';
   while(number > 0) {
       for(a in obj) { 
-          if(obj[a]<=number){ ans += a; number -= obj[a]; break;}
+          if(obj[a]<=number){
+             ans += a;
+              number -= obj[a];
+              break;
+            }
       }
   }
   return ans;
