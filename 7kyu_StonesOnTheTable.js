250df3e90fa0ccb6ecb4f05d815a75ffc69ec081
Erhan <erhantezer68@gmail.com>
2022-06-24 12:38:13 +0300

    updated

--- 7kyu_StonesOnTheTable.js
+++ 7kyu_StonesOnTheTable.js
@@ -12,7 +12,10 @@
 
 function solve(stones) {
   let count = 0;
-  for (let i = 1; i <= stones.length; i++)
-    if (stones[i] === stones[i-1]) { count++ }
+  for (let i = 1; i <= stones.length; i++){
+    if (stones[i] === stones[i-1]) {
+       count++ 
+      }
+  }
   return count;
 }
